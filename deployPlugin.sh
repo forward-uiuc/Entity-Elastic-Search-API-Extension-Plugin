ae5bcc0495bd46b27e212bdb9e82b44ef688bf6b
Long Pham <ltpham3@illinois.edu>
2018-08-21 16:13:42 -0500

    revise when writing installation guideline

--- deployPlugin.sh
+++ deployPlugin.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 ./gradlew clean assemble
-/Users/longpham/Workspace/elasticsearch-5.6.1/bin/elasticsearch-plugin remove elasticsearch-esdocumentsearch
-/Users/longpham/Workspace/elasticsearch-5.6.1/bin/elasticsearch-plugin install file:///Users/longpham/Workspace/Entity-Elastic-Search-API-Extension-Plugin/build/distributions/elasticsearch-esdocumentsearch-5.6.1.zip
+/Users/longpham/Workspace/ForwardSearchInstallationGuide/elasticsearch-5.6.1/bin/elasticsearch-plugin remove elasticsearch-esdocumentsearch
+/Users/longpham/Workspace/ForwardSearchInstallationGuide/elasticsearch-5.6.1/bin/elasticsearch-plugin install file:///Users/longpham/Workspace/ForwardSearchInstallationGuide/Entity-Elastic-Search-API-Extension-Plugin/build/distributions/elasticsearch-esdocumentsearch-5.6.1.zip
